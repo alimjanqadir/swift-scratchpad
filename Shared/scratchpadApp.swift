23f4312d7945b679e39cee04e65e90d7aeb11a20
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2021-04-02 13:29:03 +0800

    Initial Commit

---
+++ Shared/scratchpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  scratchpadApp.swift
+//  Shared
+//
+//  Created by Alimjan Qadir on 2021/4/2.
+//
+
+import SwiftUI
+
+@main
+struct scratchpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
